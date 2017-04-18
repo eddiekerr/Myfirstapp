7f04d7838057aa5b7940c570ee3c1b530d13a422
Edward Kerr <eddie@paperkite.co.nz>
2017-04-18 13:14:36 +1200

    Doing more test changes, adding mobile centre sdk

--- app/src/main/java/com/example/eddiekerr/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/eddiekerr/myfirstapp/MainActivity.java
@@ -6,12 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+import com.microsoft.azure.mobile.distribute.Distribute;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileCenter.start(getApplication(), "f259a45e-b822-45ca-a329-32451f385e88",
+                Analytics.class, Crashes.class, Distribute.class);
     }
 
     public void sendMessage (View view){
